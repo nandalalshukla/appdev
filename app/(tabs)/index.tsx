f1e87375d081fbfaa32b65b20e5100e6fadd22b0
Nandalal Shukla <shuklanandalal@gmail.com>
2026-03-15 23:23:55 +0530

    created the sticker smash app reading expo docs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,15 +3,43 @@ import CircleButton from "@/components/CircleButton";
 import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { useState, useEffect, useRef } from "react";
+import { ImageSourcePropType, StyleSheet, View, Platform } from "react-native";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiList from '@/components/EmojiList';
+import EmojiSticker from '@/components/EmojiSticker';
+import * as MediaLibrary from "expo-media-library";
+import { captureRef } from "react-native-view-shot";
+import domtoimage from "dom-to-image";
+
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+
+
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const imageRef = useRef<View>(null);
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined,
   );
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+    const [pickedEmoji, setPickedEmoji] = useState<
+      ImageSourcePropType | undefined
+      >(undefined);
+  
+  useEffect(() => {
+    if(!permissionResponse?.granted) {
+      requestPermission();
+    }
+  }, []);
+
+
+  const useImagePressed = () => {
+    setShowAppOptions(true);
+  };
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -33,18 +61,55 @@ export default function Index() {
     setShowAppOptions(false);
   };
   const onAddSticker = () => {
-    alert("You tapped on add sticker.");
+    setIsModalVisible(true);
   };
-  const onSaveImageAsync = async () => {
-    // Implementation for saving image asynchronously
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
   };
+
+    const onSaveImageAsync = async () => {
+      if (Platform.OS !== "web") {
+        try {
+          const localUri = await captureRef(imageRef, {
+            height: 440,
+            quality: 1,
+          });
+
+          await MediaLibrary.saveToLibraryAsync(localUri);
+          if (localUri) {
+            alert("Saved!");
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      } else {
+        try {
+          const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+            quality: 0.95,
+            width: 320,
+            height: 440,
+          });
+
+          let link = document.createElement("a");
+          link.download = "sticker-smash.jpeg";
+          link.href = dataUrl;
+          link.click();
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    };
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
+    <GestureHandlerRootView style={styles.container}>
+      <View ref={imageRef} collapsable={false}>
         <ImageViewer
           imgSource={PlaceholderImage}
           selectedImage={selectedImage}
-        />{" "}
+        />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        )}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -65,13 +130,13 @@ export default function Index() {
             label="Choose a photo"
             onPress={pickImageAsync}
           />
-          <Button
-            label="Use this photo"
-            onPress={() => setShowAppOptions(true)}
-          />
+          <Button label="Use this photo" onPress={useImagePressed} />
         </View>
       )}
-    </View>
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
+    </GestureHandlerRootView>
   );
 }
 
